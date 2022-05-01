2eb2ea7ed8ed607845dd8c490aa10f14d97f2329
sureshraxz <sureshkrishnawebdev@gmail.com>
2022-05-01 14:16:53 +0530

    step 4. Refactoring app header - navbar

--- ngrx-brushup/src/app/app.module.ts
+++ ngrx-brushup/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { PublicModule } from 'src/public/public.module';
 
 
 @NgModule({
@@ -12,7 +13,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PublicModule
   ],
   providers: [],
   bootstrap: [AppComponent]
